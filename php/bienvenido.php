390c188b8c69dbcbbd5caaf41abd1c019fe9d230
Estiven Morales <estivenmorales391@gmail.com>
2023-10-14 16:22:25 -0500

    actualizacion mejora de index css localmente

--- php/bienvenido.php
+++ php/bienvenido.php
@@ -26,7 +26,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="NATALIA ACOSTA, JAIDER MORALES, GABRIEL BARRERA, JULIAN GRANADOS, LUIS VARRON">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/estilobienvenido.css" >
+    <link rel="stylesheet" type="text/css" href="http://localhost/Digipets/code/css/estilobienvenido.css" >
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
     <!--LINK PARA LIBRERIA DE ICONOS -->
@@ -55,7 +55,7 @@
         <li ><a href="../servicios.html">servicios</a></li>
         <li ><a href="../Planes.html">Planes</a></li>
         <li><a  href="../Donaciones.html">Donaciones</a></li> 
-        <li ><a href="petshop.php">PetShop</a></li>
+        <li ><a href="../PetShop.html">PetShop</a></li>
         <li><a href="ReporteUsuarios.php" class="btn-small blue z-depth-2">Reporte</a></li>
     </ul>
     <!--GIF DE CASA COMPRAS-->
--- php/conexion.php
+++ php/conexion.php
@@ -2,10 +2,10 @@
 //Configuracion necesaria para acceder a la base de datos
 
 function conn(){
-   $hostname = "localhost";
-   $usuariodb = "edigipee8_Admin";
-   $passworddb = "euxdalyjze25691";
-   $dbname = "edigipee8_Digipets"; 
+   $hostname = "Localhost";
+   $usuariodb = "root";
+   $passworddb = "";
+   $dbname = "digipets"; 
 
    // Generardo la conexion con el servidor
    $conectar = mysqli_connect($hostname, $usuariodb, $passworddb, $dbname);
