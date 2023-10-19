2c5cfda9a445d361f7757b729d73afacf3e22222
Camilo Lopez <126996964+Mailo477@users.noreply.github.com>
2023-10-18 19:33:40 -0500

    Prueba 4

--- js/ScrollProcesarCita.js
+++ js/ScrollProcesarCita.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
         origin: 'top',
         distance: '20px',
         duration: 1000,
-        delay: 600, 
+        delay: 500, 
         easing: 'ease-in-out',
         reset: true
     });
