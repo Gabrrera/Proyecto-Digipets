aa5b13bdb69e76cd5f349044991ecfe703647d64
Gabrrera <6wmwm9@gmail.com>
2023-10-23 18:36:23 -0500

    Se modifica la base de datos

--- php/conexion.php
+++ php/conexion.php
@@ -2,10 +2,10 @@
 //Configuracion necesaria para acceder a la base de datos
 
 function conn(){
-   $hostname = "Localhost";
-   $usuariodb = "root";
-   $passworddb = "";
-   $dbname = "digipets"; 
+   $hostname = "localhost";
+   $usuariodb = "edigipee8_QA";
+   $passworddb = "euxdalyjze25691";
+   $dbname = "edigipee8_Digipets_QA"; 
 
    // Generardo la conexion con el servidor
    $conectar = mysqli_connect($hostname, $usuariodb, $passworddb, $dbname);
