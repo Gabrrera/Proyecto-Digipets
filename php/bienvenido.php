b054242e7ac8d8308ca14deec2d8b3bc597b2c93
Gabrrera <6wmwm9@gmail.com>
2023-10-23 19:06:40 -0500

    Restructuracion bienvenido.php

--- php/bienvenido.php
+++ php/bienvenido.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="NATALIA ACOSTA, JAIDER MORALES, GABRIEL BARRERA, JULIAN GRANADOS, LUIS VARRON"> 
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/estilobienvenido">
+    <link rel="stylesheet" type="text/css" href="../css/estilobienvenido.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
     <!--LINK PARA LIBRERIA DE ICONOS -->
